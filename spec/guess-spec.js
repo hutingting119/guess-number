28ab3546991405336b5dad712eadc5b89bf706b5
hutingting119 <1912257350@qq.com>
2016-07-26 19:23:40 +0800

    build guess-spec

---
+++ spec/guess-spec.js
@@ -0,0 +1,13 @@
+'use strict'
+//const app = require('../src/app');
+
+const AnswerGenerator = require('../src/models/answerGenerator');
+const Guess = require('../src/models/guess');
+
+describe('Guess', ()=> {
+    it('should play the game', ()=> {
+        spyOn(AnswerGenerator, 'answer').and.returnValue('1234');
+        const result = Guess.guess('1234');
+        expect(result).toEqual('4A0B');
+    })
+})
